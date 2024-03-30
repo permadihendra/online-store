7939c560fd4c8ea3b44bc7760d306d6d7fddd1c3
permadihendra <26792223+permadihendra@users.noreply.github.com>
2024-03-30 22:38:49 +0700

    Convert NavBar to livewire component

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -27,31 +27,8 @@
         </div>
     </nav> --}}
 
-    <nav class="navbar navbar-expand-sm bg-secondary">
-        <div class="container">
-            <a class="navbar-brand text-white" href="#">Navbar</a>
-            <button class="navbar-toggler" data-bs-theme="dark" type="button" data-bs-toggle="offcanvas"
-                data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu" aria-expanded="false">
-                <span class="navbar-toggler-icon "></span>
-            </button>
-            <div class="offcanvas offcanvas-end px-3 w-50 text-white bg-secondary" tabindex="-1" id="offcanvasMenu"
-                aria-labelledby="offcanvasMenu">
-                <div class="offcanvas-header" data-bs-theme="dark">
-                    <h5 class="offcanvas-title" id="offcanvasMenu">Menu</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <div class="ms-auto">
-                        <div class="navbar-nav ms-auto">
-
-                            <a class="nav-link text-white active" aria-current="page" href="#">Home</a>
-                            <a class="nav-link text-white" href="#">About</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </nav>
+    // NavBar livewire Component
+    <livewire:Homepage.NavBar />
 
     <header class="masthead text-white text-center py-4 bg-primary">
         <div class="container d-flex flex-column align-items-center">
